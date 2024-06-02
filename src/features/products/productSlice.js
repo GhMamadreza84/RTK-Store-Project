0aa845fe5d2472972650a8bb541c86fc51340cb1
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-02 11:16:58 -0700

    import api from config.js and return  in fetchProducts

--- src/features/products/productSlice.js
+++ src/features/products/productSlice.js
@@ -1,7 +1,13 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+import api from "../../services/config";
 
 const initialState = {
   loading: false,
   products: [],
   error: "",
 };
+
+const fetchProducts = createAsyncThunk("product/fetchProducts", () => {
+  return api;
+});
