0e87b033843445b5248b607b77d98d6d9723caac
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-02 12:08:19 -0700

    wrap store in main with provider from react-redux

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./global.css";
-
+import store from "./app/store.js";
+import { Provider } from "react-redux";
 ReactDOM.createRoot(document.getElementById("root")).render(
    // <React.StrictMode>
     <BrowserRouter>
+    <Provider store={store} >
+
       <App />
+    </Provider>
     </BrowserRouter>
   // </React.StrictMode>
 );
