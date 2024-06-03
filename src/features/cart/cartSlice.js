537ea91045ff2236b472e26c2b8414bfb09adbb2
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-03 02:39:46 -0700

    create cartSlice and import createSlice from RTK

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  selectedItems: [],
+  itemsCounter: 0,
+  total: 0,
+  checkout: false,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem: (state, action) => {
+      if (!state.selectedItems.find((i) => i.id === action.payload.id)) {
+      }
+    },
+  },
+});
