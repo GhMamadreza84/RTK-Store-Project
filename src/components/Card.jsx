f63b3b3d2840d33b96473346eb82cc21398c2281
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-03 03:18:11 -0700

    log from state in card for find bug

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -17,9 +17,8 @@ const Card = ({ data }) => {
 
   const dispatch = useDispatch();
   const state = useSelector((store) => store.cart);
-
+ console.log(state)
   const quantity = productQuantity(state, id);
-
   return (
     <div className={styles.card}>
       <img src={image} alt={title} />
@@ -31,20 +30,20 @@ const Card = ({ data }) => {
         </Link>
         <div>
           {quantity === 1 && (
-            <button onClick={() => dispatch(removeItem())}>
+            <button onClick={() => dispatch(removeItem(data))}>
               <MdDeleteOutline />
             </button>
           )}
           {quantity > 1 && (
-            <button onClick={() => dispatch(decrease())}>-</button>
+            <button onClick={() => dispatch(decrease(data))}>-</button>
           )}
           {!!quantity && <span>{quantity}</span>}
           {quantity === 0 ? (
-            <button onClick={() => dispatch(addItem())}>
+            <button onClick={() => dispatch(addItem(data))}>
               <TbShoppingBagCheck />
             </button>
           ) : (
-            <button onClick={() => dispatch(increase())}>+</button>
+            <button onClick={() => dispatch(increase(data))}>+</button>
           )}
         </div>
       </div>
